1194e48c56fab467375e36cb7822c3c5f03ee193
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2025-02-06 15:32:19 -0300

    primeiro teste

--- tests/unit/services/CustomerService.test.js
+++ tests/unit/services/CustomerService.test.js
@@ -1,5 +1,55 @@
-const sum = (a, b) => a + b
+const { CustomerService } = require("../../../dist/services/CustomerService")
 
-test("adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3)
+const correctBody = {
+    fullName: "Mickey Mouse",
+    birthday: "1999-05-19",
+    active: true,
+    addresses: [
+        {
+            postalCode: "99700114",
+            state: "RS",
+            city: "Erechim",
+            neighborhood: "Centro",
+            street: "Avenida Pedro Pinto de Souza",
+            streetNumber: 199,
+            complement: "Perto da agencia do mercado livre.",
+        },
+    ],
+    emailAddresses: [
+        {
+            isMain: true,
+            email: "inazumaseleven04@gmaill.com",
+        },
+        {
+            isMain: false,
+            email: "igoralmeida@gmail.com",
+        },
+    ],
+    phoneNumbers: [
+        {
+            isMain: false,
+            phoneNumber: "+55 54 9985-6043",
+        },
+        {
+            isMain: true,
+            phoneNumber: "+55 54 9975-6043",
+        },
+    ],
+}
+
+describe("CustomerService tests", () => {
+    test("should create a new user", async () => {
+        const customerService = new CustomerService({
+            create: async () => "123",
+        })
+
+        const result = await customerService.create(correctBody)
+
+        expect(result).toEqual({
+            success: true,
+            data: {
+                id: "123",
+            },
+        })
+    })
 })
