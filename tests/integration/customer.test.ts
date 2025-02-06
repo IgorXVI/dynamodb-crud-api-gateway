376ea08933247d1331c19b8fb7c974ef660a2350
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2025-02-06 17:45:46 -0300

    adicionando mais testes de integracao para as operacoes crud novas

--- tests/integration/customer.test.ts
+++ tests/integration/customer.test.ts
@@ -1,7 +1,8 @@
 const correctBody = {
+    id: "",
+    active: true,
     fullName: "Mickey Mouse",
     birthday: "1999-05-19",
-    active: true,
     addresses: [
         {
             postalCode: "99700114",
@@ -49,6 +50,7 @@ describe("Customer routes tests", () => {
 
         expect(resBody.success).toEqual(true)
         expect(typeof resBody.data.id).toEqual("string")
+        correctBody.id = resBody.data.id
     })
 
     test("should get all customers", async () => {
@@ -63,8 +65,63 @@ describe("Customer routes tests", () => {
 
         expect(resBody.success).toEqual(true)
 
-        delete resBody.data.items[0].id
+        expect(resBody.data.items.find((item: any) => item.id === correctBody.id)).toEqual(correctBody)
+    })
 
-        expect(resBody.data.items[0]).toEqual(correctBody)
+    test("should update one costumer", async () => {
+        correctBody.fullName = "Igor"
+
+        const res = await fetch(`http://localhost:3000/dev/customer/${correctBody.id}`, {
+            method: "PUT",
+            body: JSON.stringify(correctBody),
+            headers: {
+                "x-api-key": "d41d8cd98f00b204e9800998ecf8427e",
+            },
+        })
+
+        const resBody = await res.json()
+
+        expect(resBody.success).toEqual(true)
+    })
+
+    test("should get one costumer", async () => {
+        const res = await fetch(`http://localhost:3000/dev/customer/${correctBody.id}`, {
+            method: "GET",
+            headers: {
+                "x-api-key": "d41d8cd98f00b204e9800998ecf8427e",
+            },
+        })
+
+        const resBody = await res.json()
+
+        expect(resBody.success).toEqual(true)
+        expect(resBody.data).toEqual(correctBody)
+    })
+
+    test("should delete one costumer", async () => {
+        const res = await fetch(`http://localhost:3000/dev/customer/${correctBody.id}`, {
+            method: "DELETE",
+            headers: {
+                "x-api-key": "d41d8cd98f00b204e9800998ecf8427e",
+            },
+        })
+
+        const resBody = await res.json()
+
+        expect(resBody.success).toEqual(true)
+    })
+
+    test("should get one costumer that is innactive and receive a not found error", async () => {
+        const res = await fetch(`http://localhost:3000/dev/customer/${correctBody.id}`, {
+            method: "GET",
+            headers: {
+                "x-api-key": "d41d8cd98f00b204e9800998ecf8427e",
+            },
+        })
+
+        const resBody = await res.json()
+
+        expect(resBody.success).toEqual(false)
+        expect(resBody.errorMessage).toEqual("Customer not found.")
     })
 })
