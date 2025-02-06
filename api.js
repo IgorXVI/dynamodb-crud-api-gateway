4d30afc91c3d9027629d61b87ec5828ba145f0ea
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2025-02-06 13:25:06 -0300

    fazendo o controller

--- api.js
+++ api.js
@@ -1,52 +1,12 @@
 const { CustomerRepository } = require("./src/repositories/CustomerRepository")
 const { CustomerService } = require("./src/services/CustomerService")
+const { CustomerController } = require("./src/controllers/CustomerController")
 
 const customerRepository = new CustomerRepository()
 const customerService = new CustomerService(customerRepository)
-
-const errorHandlingWrapper = (fun) => async (event) => {
-    try {
-        const normalResponse = await fun(event)
-        return normalResponse
-    } catch (e) {
-        console.error(e)
-
-        const errorResponse = {}
-
-        errorResponse.statusCode = 500
-
-        errorResponse.body = JSON.stringify({
-            success: false,
-            errorMessage: e.message ? e.message : "Error has no message!",
-        })
-
-        return errorResponse
-    }
-}
-
-const createCustomer = errorHandlingWrapper(async (event) => {
-    const body = JSON.parse(event.body)
-
-    const result = await customerService.create(body)
-
-    const statusCode = result.success ? 201 : 400
-
-    return {
-        statusCode,
-        body: JSON.stringify(result),
-    }
-})
-
-const getAllCustomers = errorHandlingWrapper(async () => {
-    const result = await customerService.getAll()
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(result),
-    }
-})
+const customerController = new CustomerController(customerService)
 
 module.exports = {
-    createCustomer,
-    getAllCustomers,
+    createCustomer: customerController.create,
+    getAllCustomers: customerController.getAll,
 }
---
+++ src/controllers/CustomerController.js
@@ -0,0 +1,52 @@
+class CustomerController {
+    constructor(customerService) {
+        this.customerService = customerService
+    }
+
+    handleError(e) {
+        console.error(e)
+
+        const errorResponse = {}
+
+        errorResponse.statusCode = 500
+
+        errorResponse.body = JSON.stringify({
+            success: false,
+            errorMessage: e.message ? e.message : "Error has no message!",
+        })
+
+        return errorResponse
+    }
+
+    async create(event) {
+        try {
+            const body = JSON.parse(event.body)
+
+            const result = await customerService.create(body)
+
+            const statusCode = result.success ? 201 : 400
+
+            return {
+                statusCode,
+                body: JSON.stringify(result),
+            }
+        } catch (e) {
+            this.handleError(e)
+        }
+    }
+
+    async getAll() {
+        try {
+            const result = await customerService.getAll()
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify(result),
+            }
+        } catch (e) {
+            this.handleError(e)
+        }
+    }
+}
+
+module.exports = { CustomerController }
